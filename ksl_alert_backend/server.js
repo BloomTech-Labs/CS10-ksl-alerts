2315cb9bdfab9f0ca3547a8aa4d75a93f238d2df
JSNavey <jsnavey@gmail.com>
2018-09-12 12:42:22 -0400

    Set up database Create ksl user database Initialize hash password Create user model Add user controller file

--- ksl_alert_backend/server.js
+++ ksl_alert_backend/server.js
@@ -9,20 +9,22 @@ const middleware = require('./utils/middleware.js');
 const server = express();
 middleware(server);
 
-
 server.listen(`${PORT}`, () =>
-     console.log(`\n=== API running on port ${PORT} ===\n`)
+      console.log(`\n=== API running on port ${PORT} ===\n`)
 )
-/* WIP until we have our controller and schema built
-database
- .connectTo("test")
- .then(() => {
-   console.log(“\n... API Connected to lambda-Notes Database ...\n”);
-   server.listen(`${port}`, () =>
-     console.log(`\n=== API running on port ${port} ===\n`)
-   );
- })
- .catch(err => {
-   console.log(“\n*** ERROR Connecting to MongoDB, is it running? ***\n”, err);
- });
-*/
+
+
+/* WIP until we have our controller and schema built */
+
+// database
+//   .connectTo("test")
+//   .then(() => {
+//     console.log(`\n... API Connected to lambda-Notes Database ...\n`);
+//     server.listen(`${PORT}`, () =>
+//       console.log(`\n=== API running on port ${PORT} ===\n`)
+//     );
+//   })
+//   .catch(err => {
+//     console.log(`\n*** ERROR Connecting to MongoDB, is it running? ***\n`, err);
+//   });
+
