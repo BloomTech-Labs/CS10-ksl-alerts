cadfbddad61c47fe14aacf6174e4ddf585d0ae2d
maxwashington <mnwashington18@gmail.com>
2018-09-12 15:54:30 -0500

    Create user signin validation passwords test signin error test signup error test db connection

--- ksl_alert_backend/users/userController.js
+++ ksl_alert_backend/users/userController.js
@@ -1,33 +1,66 @@
 //user route middleware
-const router = require('express').Router();
-const jwt = require('jsonwebtoken');
-const User = require('./userModel.js');
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const User = require("./userModel.js");
 
-router
-  .route('/')
-  .get((req, res) => {
-    User.find()
-        .then(users => {
-          res.status(200).json(users);
-        })
-        .catch(err => {
-          res.status(500).json({ errorMessage: "Error fetching users" });
-        });
-  });
+const secret = "Something here";
 
-router
-  .route('/signup')
-  .post((req, res) => {
-    const { email, password } = req.body;
-    const newUser = new User({ email, password });
-    newUser
-      .save()
-      .then(savedUser => {
-        res.status(201).json(savedUser);
-      })
-      .catch(err => {
-        res.status(422).json({ errorMessage: "Error creating user" });
-      });
+//generate token for the login
+const generateToken = user => {
+  const options = {
+    expiresIn: "24h"
+  };
+  const payload = {};
+  return jwt.sign(payload, secret, options);
+};
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Error fetching users" });
+    });
+});
+
+router.route("/signUp").post((req, res) => {
+  const { email, password } = req.body;
+  const newUser = new User({ email, password });
+  newUser
+    .save()
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(err => {
+      res.status(422).json({ errorMessage: "Error creating user" });
+    });
+});
+
+router.route("/signIn").post((req, res) => {
+  const { email, password } = req.body;
+  User
+    .findOne({ email })
+    .then(user => {
+        if(user) {
+          user.validatePassword(password)
+            .then(match => {
+              if(match) {
+                const token = generateToken({ email });
+                res.status(200).json({ message: `Welcome back ${ email }`, token });
+              } else {
+                res.status(404).json({ errorMessage: "Invalid email or password" });
+              }
+            })
+            .catch(err => {
+              res.status(500).json({ errorMessage: "Error", err});
+            });
+        }
+    })
+    .catch(err => {
+      res.status(404).json({ errorMessage: "User not found"});
+    });
 });
 
 module.exports = router;
+
