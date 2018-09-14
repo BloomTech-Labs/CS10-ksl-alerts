e6fb8c553a129b3df2c4c517aaae368c886cd8a0
Tyson Williams <tyson.j.williams@gmail.com>
2018-09-14 11:03:18 -0600

    Update AlertFeed Create dummy data to test with AlertFeed Add dummy data to render with AlertFeed (WIP)

--- ksl_alert_frontend/src/components/AlertFeed/AlertFeed.js
+++ ksl_alert_frontend/src/components/AlertFeed/AlertFeed.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
-import TopNav from "../TopNav/TopNav.js";
+// import { PromiseProvider } from "mongoose";
+import alerts from "../../dummyData.js";
 
 /**
- * AlertFeed States: 
- * Signed In: Managed through app.js, the alert feed is only rendered if props.login is true, 
+ * AlertFeed States:
+ * Signed In: Managed through app.js, the alert feed is only rendered if props.login is true,
  * and they've routed to that page (after signing up/signing in, or clicking the alert feed button).
  * The alert feed will have its own local state (state.alerts) --we need to determine how to render--
  **/
 export default class AlertFeed extends Component {
-    render() {
-        return (
-            <div>
-                <p>Alert Feed</p>
-            </div>
-    )   ;
-    }
+  state = {
+    alerts: []
+  };
+
+  componentDidMount() {
+    //make call to db to get user object
+    //save user's query links to a 'queries' variable
+    //pass 'queries' into webScraper function
+    //save webScraper's return value into an 'alerts' variable
+    //pass 'alerts' variable into AlertFeed's state via setState()
+
+    //for testing: get alerts from dummyData and pass into AlertFeed's state via setState()
+    this.setState({ alerts: alerts });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.alerts.map(alert => {
+          <li>{alert}</li>
+        })}
+      </ul>
+    );
+  }
 }
---
+++ ksl_alert_frontend/src/dummyData.js
@@ -0,0 +1,17 @@
+const alerts = [
+  {
+    name: "dresser 1",
+    price: 200,
+    category: "dresser",
+  },
+  {
+    name: "dresser 2",
+    price: 250,
+    category: "dresser",
+  },
+  {
+    name: "table 1",
+    price: 150,
+    category: "table",
+  },
+];
