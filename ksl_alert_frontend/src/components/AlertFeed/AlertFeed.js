9c42e9c1041d084c5ca718d5e7aa080cd0556f4e
JSNavey <jsnavey@gmail.com>
2018-09-16 22:23:58 -0400

    Implement sign out in Alert Feed to check if token remove from local storage

--- ksl_alert_frontend/src/components/AlertFeed/AlertFeed.js
+++ ksl_alert_frontend/src/components/AlertFeed/AlertFeed.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
-import TopNav from "../TopNav/TopNav.js";
+import axios from 'axios';
 
+
+
+// use users for now. It needs to change to be saved urls
+// then scrape the saved url to show alert feed
 export default class AlertFeed extends Component {
-    render() {
-        return (
-            <div>
-                <p>Alert Feed</p>
-            </div>
-    )   ;
-    }
+	state = {
+		users: []
+	}
+
+	componentDidMount() {
+		const token = localStorage.getItem('jwt');
+		const requestOptions = {
+			headers: {
+				Authorization: token
+			}
+		};
+
+		// call axios.get to check if can fetch all users. JUST FOR TESTING
+		axios
+			.get('http://localhost:8000/api/users', requestOptions)
+			.then(res => {
+				this.setState({ users: res.data });
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	// remove token from local storage
+	signOut = () => {
+		if(localStorage.getItem('jwt')) {
+			localStorage.removeItem('jwt');
+			this.props.history.push('/');
+		}
+	}
+
+  render() {
+    return (
+      <div>
+        <p>Alert Feed</p>
+				<button onClick={this.signOut}>Sign out</button>
+      </div>
+    );
+  }
 }
