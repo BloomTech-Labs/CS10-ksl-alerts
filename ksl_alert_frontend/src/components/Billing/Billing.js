20acdd47fc5fb2db6af969129d6c57b92b735a30
maxwashington <mnwashington18@gmail.com>
2018-09-18 11:47:40 -0500

    Minor formatting to Billing,js

--- ksl_alert_frontend/src/components/Billing/Billing.js
+++ ksl_alert_frontend/src/components/Billing/Billing.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import BillingForm from "../Forms/BillingForm";
 
 export default class Billing extends Component {
-    render() {
-        return (
-            <div>
-                <p>Payment Info</p>
-                <input type="text" name="cc" placeholder="CC#" />
-                <input type="text" name="exp" placeholder="EXP" />
-                <input type="text" name="cvv" placeholder="CVV" />
-                <input type="checkbox" name="oneMonthSub">
-                    1 Month Subs - $20
-                </input>
-                <input type="checkbox" name="newPassword">
-                    1 Client - $1.99
-                </input>
-                <button>Buy Now!</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>Payment Info</p>
+        <input type="text" name="cc" placeholder="CC#" />
+        <input type="text" name="exp" placeholder="EXP" />
+        <input type="text" name="cvv" placeholder="CVV" />
+        <input type="checkbox" name="oneMonthSub">
+          1 Month Subs - $20
+        </input>
+        <input type="checkbox" name="newPassword">
+          1 Client - $1.99
+        </input>
+        <StripeProvider apiKey="pk_test_8PEVRGD76L9FgpaQaNZiCgRG">
+          <div className="billing-form">
+            <Elements>
+              <BillingForm />
+            </Elements>
+          </div>
+        </StripeProvider>
+      </div>
+    );
+  }
 }
--- ksl_alert_frontend/src/components/Forms/BillingForm.js
+++ ksl_alert_frontend/src/components/Forms/BillingForm.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
-import React, { Component } from 'react'
 
 class BillingForm extends Component {
-    constructor(props) {
-        super(props);
-        this.submit = this.submit.bind(this);
-    }
-    async submit(ev){
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {complete: false};
+    this.submit = this.submit.bind(this);
+  }
+  async submit(ev) {
+    let { token } = await this.props.stripe.createToken({ name: "Name" });
+    let response = await fetch("/api/billing", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+  }
   render() {
     return (
-        <div className="checkout">
+      <div className="checkout">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
         <button onClick={this.submit}>Send</button>
       </div>
-    )
+    );
   }
 }
 
