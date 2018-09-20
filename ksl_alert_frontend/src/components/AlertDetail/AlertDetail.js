8c5b3d1f6f9255da4a25e23c7f3fb70c0a4fa561
Tyson Williams <tyson.j.williams@gmail.com>
2018-09-20 15:40:38 -0600

    Code formatting and moved SignOut component folder

--- ksl_alert_frontend/src/components/AlertDetail/AlertDetail.js
+++ ksl_alert_frontend/src/components/AlertDetail/AlertDetail.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react'
-import cheerio from 'cheerio'
-import request from 'request'
+import React, { Component } from "react";
+import cheerio from "cheerio";
+import request from "request";
 
 class AlertDetail extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
-      url: 'https://classifieds.ksl.com/search/?keyword=guitar&zip=84101&miles=25&priceFrom=%240&priceTo=%24100&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0',
-      title: 'Guitar',
-      listings: []
-    }
+      url:
+        "https://classifieds.ksl.com/search/?keyword=guitar&zip=84101&miles=25&priceFrom=%240&priceTo=%24100&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0",
+      title: "Guitar",
+      listings: [],
+    };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     request(this.state.url, (error, response, body) => {
-      console.log('error:', error)
-      console.log('statusCode:', response && response.statusCode)
+      console.log("error:", error);
+      console.log("statusCode:", response && response.statusCode);
 
-      const $ = cheerio.load(body)
+      const $ = cheerio.load(body);
 
       // from view-source page, the data we need consist in script tag inside 'window.renderSearchSection'
-      const scripts = $('script').toArray()
+      const scripts = $("script").toArray();
 
       scripts.find(script => {
         if (script.children[0] !== undefined) {
           if (script.children[0].data !== undefined) {
             if (
-              script.children[0].data.includes('window.renderSearchSection')
+              script.children[0].data.includes("window.renderSearchSection")
             ) {
-              let searchResults = script.children[0].data
+              let searchResults = script.children[0].data;
               // change the format to be json
-              const startIndex = searchResults.indexOf('(')
-              let results = eval(searchResults.substring(startIndex))
-              this.setState({ listings: results.listings })
+              const startIndex = searchResults.indexOf("(");
+              let results = eval(searchResults.substring(startIndex));
+              this.setState({ listings: results.listings });
               // console.log(results);
             }
           }
         }
-      })
-    })
+      });
+    });
   }
 
-  render () {
+  render() {
     return (
       <div>
         <h3>Alert Detail!</h3>
@@ -54,11 +55,11 @@ class AlertDetail extends Component {
               <p>{listing.createTime}</p>
               <img src={`https://${listing.photo.slice(2)}`} />
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default AlertDetail
+export default AlertDetail;
