4e4d0e6edb80db4375b3c9845acaa4e7f1237a7d
JSNavey <jsnavey@gmail.com>
2018-09-20 17:04:59 -0400

    Receiving parsed data from KSL Rendering in AlertDetail component CORS issue fixed by installing Allow-Control-Allow-Origin extension.

--- ksl_alert_frontend/src/components/AlertDetail/AlertDetail.js
+++ ksl_alert_frontend/src/components/AlertDetail/AlertDetail.js
@@ -1,60 +1,64 @@
-import React, { Component } from 'react';
-import cheerio from 'cheerio';
-import request from 'request';
+import React, { Component } from 'react'
+import cheerio from 'cheerio'
+import request from 'request'
 
 class AlertDetail extends Component {
-	constructor() {
-		super();
-		this.state = {
-			url: 'https://classifieds.ksl.com/search/?keyword=guitar&zip=84101&miles=25&priceFrom=%240&priceTo=%24100&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0',
-			title: 'Guitar'
-		}
-	}
+  constructor () {
+    super()
+    this.state = {
+      url: 'https://classifieds.ksl.com/search/?keyword=guitar&zip=84101&miles=25&priceFrom=%240&priceTo=%24100&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0',
+      title: 'Guitar',
+      listings: []
+    }
+  }
 
-	componentDidMount() {
-		fetch('https://www.ksl.com', {mode:'no-cors'}).then(res => {
-			console.log('res:', res.body);
-		})
-		// const options = {
-		// 	url: this.state.url,
-		// 	headers: {
-		// 		'Access-Control-Allow-Origin': '*'
-		// 	},
-		// 	method: 'GET'
-		// }
+  componentDidMount () {
+    request(this.state.url, (error, response, body) => {
+      console.log('error:', error)
+      console.log('statusCode:', response && response.statusCode)
 
-		// request(options, (error, response, body) => {
-		// 	console.log('error:', error);
-		// 	console.log('statusCode:', response && response.statusCode);
-		// 	// console.log('body:', body);
-		
-		// 	const $ = cheerio.load(body);
-		
-		// 	// from view-source page, the data we need consist in script tag inside 'window.renderSearchSection'
-		// 	const scripts = $('script').toArray();
-		
-		// 	scripts.find(script => {
-		// 		if (script.children[0] !== undefined) {
-		// 			if (script.children[0].data !== undefined)
-		// 				if (script.children[0].data.includes('window.renderSearchSection')) {
-		// 					let searchResults = script.children[0].data
-		// 					// change the format to be json
-		// 					const startIndex = searchResults.indexOf('(')
-		// 					let results = eval(searchResults.substring(startIndex))
-		// 					console.log(results);
-		// 				}
-		// 		}
-		// 	});
-		// });
-	}
+      const $ = cheerio.load(body)
 
-  render() {
+      // from view-source page, the data we need consist in script tag inside 'window.renderSearchSection'
+      const scripts = $('script').toArray()
+
+      scripts.find(script => {
+        if (script.children[0] !== undefined) {
+          if (script.children[0].data !== undefined) {
+            if (
+              script.children[0].data.includes('window.renderSearchSection')
+            ) {
+              let searchResults = script.children[0].data
+              // change the format to be json
+              const startIndex = searchResults.indexOf('(')
+              let results = eval(searchResults.substring(startIndex))
+              this.setState({ listings: results.listings })
+              // console.log(results);
+            }
+          }
+        }
+      })
+    })
+  }
+
+  render () {
     return (
       <div>
-          Alert Detail!!!     
+        <h3>Alert Detail!</h3>
+        {this.state.listings.map(listing => {
+          return (
+            <div>
+              <p>Item name: {listing.title}</p>
+              <p>{listing.price}</p>
+              <p>{listing.city}</p>
+              <p>{listing.createTime}</p>
+              <img src={`https://${listing.photo.slice(2)}`} />
+            </div>
+          )
+        })}
       </div>
-  	);
+    )
   }
 }
 
-export default AlertDetail;
+export default AlertDetail
