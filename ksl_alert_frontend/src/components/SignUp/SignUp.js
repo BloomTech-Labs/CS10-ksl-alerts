291c4b4f4274e81e20ac40296978e2903bf9860f
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-19 18:28:14 -0700

    Add ability to sign up new user in signUp component

--- ksl_alert_frontend/src/components/SignUp/SignUp.js
+++ ksl_alert_frontend/src/components/SignUp/SignUp.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SignUp extends Component {
-    render() {
-        return (
-            <div>
-                Sign Up
-            </div>
-        );
-    }
+  state = {
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8000/api/user/signUp', this.state)
+      .then(res => {
+        console.log("Sign up successful")
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Sign Up</h3>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleInput}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handleInput}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={this.state.confirmPassword}
+          onChange={this.handleInput}
+        />
+        <button onClick={this.submitForm}>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
