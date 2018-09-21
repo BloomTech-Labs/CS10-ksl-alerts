695241d3b3afb34b11ea79daed7d651590b1cfb4
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-20 23:04:19 -0700

    Add UI elements to Settings page
    - Add input, label and button for Update Email
    - Add input, label and button for Update Password
    - Add semantic-ui-css dependency
    - Import semantic-ui-css in App.js
    - Basic styling for Settings page

--- ksl_alert_frontend/src/components/Settings/Settings.js
+++ ksl_alert_frontend/src/components/Settings/Settings.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Button, Input, Form } from 'semantic-ui-react';
+import './Settings.css';
 
 export default class Settings extends Component {
   render() {
     return (
-      <div>
+      <div className="PageContainer">
         <h2>Settings</h2>
-        <p>Settings would be here</p>
+        <div className="SettingsContainer">
+          <Form className="UpdateEmailForm">
+            <label>Update User Info</label>
+            <Form.Field>
+              <Input name="email" placeholder="Updated Email Address" />
+            </Form.Field>
+            <Button type="submit">Submit</Button>
+          </Form>
+
+          <Form className="UpdatePasswordForm">
+            <label>Update Password</label>
+            <Form.Field>
+              <Input name="currentPassword" placeholder="Current Password" />
+            </Form.Field>
+            <Form.Field>
+              <Input name="newPassword" placeholder="New Password" />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                name="confirmNewPassword"
+                placeholder="Confirm New Password"
+              />
+            </Form.Field>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </div>
       </div>
     );
   }
