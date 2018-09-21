790168e2ec0caab8755e87a13994ef3f8a8e8a58
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-21 00:09:05 -0700

    Add handler functions for in Settings.js
    - Add handleInput and pass to Input fields' onChange property
    - Add handleSubmitEmail stub and pass to submit button's onClick property
    - Add handleSubmitPassword stub and pass to submit button's onClick property
    TODO: Write endpoint(s) in back-end for updating user info

--- ksl_alert_frontend/src/components/Settings/Settings.js
+++ ksl_alert_frontend/src/components/Settings/Settings.js
@@ -3,6 +3,29 @@ import { Button, Input, Form, Label } from 'semantic-ui-react';
 import './Settings.css';
 
 export default class Settings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      currentPassword: '',
+      newPassword: '',
+      confirmNewPassword: ''
+    };
+  }
+
+  handleSubmitEmail = e => {
+    console.log(this.state);
+  };
+
+  handleSubmitPassword = e => {
+    console.log(this.state);
+  };
+
+  handleInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <div className="PageContainer">
@@ -11,26 +34,43 @@ export default class Settings extends Component {
           <Form className="UpdateEmailForm">
             <Label>Update User Info</Label>
             <Form.Field>
-              <Input name="email" placeholder="Updated Email Address" />
+              <Input
+                name="email"
+                placeholder="Updated Email Address"
+                value={this.state.email}
+                onChange={this.handleInput}
+              />
             </Form.Field>
-            <Button type="submit">Submit</Button>
+            <Button onClick={this.handleSubmitEmail}>Submit</Button>
           </Form>
 
           <Form className="UpdatePasswordForm">
             <Label>Update Password</Label>
             <Form.Field>
-              <Input name="currentPassword" placeholder="Current Password" />
+              <Input
+                name="currentPassword"
+                placeholder="Current Password"
+                value={this.state.currentPassword}
+                onChange={this.handleInput}
+              />
             </Form.Field>
             <Form.Field>
-              <Input name="newPassword" placeholder="New Password" />
+              <Input
+                name="newPassword"
+                placeholder="New Password"
+                value={this.state.newPassword}
+                onChange={this.handleInput}
+              />
             </Form.Field>
             <Form.Field>
               <Input
                 name="confirmNewPassword"
                 placeholder="Confirm New Password"
+                value={this.state.confirmNewPassword}
+                onChange={this.handleInput}
               />
             </Form.Field>
-            <Button type="submit">Submit</Button>
+            <Button onClick={this.handleSubmitPassword}>Submit</Button>
           </Form>
         </div>
       </div>
