cec4110061686232ebd4d63b2337bbf586320cd8
maxwashington <mnwashington18@gmail.com>
2018-09-25 18:49:52 -0500

    Stripe route is up testing to comence

--- ksl_alert_frontend/src/components/Billing/Stripe/stripe.js
+++ ksl_alert_frontend/src/components/Billing/Stripe/stripe.js
@@ -3,7 +3,7 @@ import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 
 const CURRENCY = "USD";
-const PAYMENT_SERVER_URL = process.env.NODE_ENV === "http://localhost:8000";
+const PAYMENT_SERVER_URL = 'http://localhost:8000/payments'
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === "pk_test_8PEVRGD76L9FgpaQaNZiCgRG";
 
 const fromDollarToCent = amount => amount * 100;
