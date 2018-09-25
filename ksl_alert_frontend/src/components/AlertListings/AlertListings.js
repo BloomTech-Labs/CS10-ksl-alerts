6d02ffd72e749c8561b2f288be2b0c4b1c8367ec
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-25 15:11:00 -0700

    Prevent App from crashing with invalid URL's
    modified:   src/components/AlertListings/AlertListings.js
      - Add an `if` statement that only attempts to scrape query links if `body` exists
      - Format code
      TODO: Add a way to indicate query link is invalid in the front-end

--- ksl_alert_frontend/src/components/AlertListings/AlertListings.js
+++ ksl_alert_frontend/src/components/AlertListings/AlertListings.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import cheerio from "cheerio";
-import request from "request";
+import React, { Component } from 'react';
+import cheerio from 'cheerio';
+import request from 'request';
 import ListingCard from '../ListingCard/ListingCard.js';
 
 // receiving query props from AlertCard which has query title and url
@@ -8,39 +8,43 @@ class AlertListings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listings: [],
+      listings: []
     };
   }
 
   componentDidMount() {
     // using request library to get url(s) from our user database with specific user id
     request(this.props.url, (error, response, body) => {
-      console.log("error:", error);
-      console.log("statusCode:", response && response.statusCode);
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
 
-      // passing the body to cheerio
-      const $ = cheerio.load(body);
+      if (body) {
+        // passing the body to cheerio
+        const $ = cheerio.load(body);
 
-      // from view-source page, the data we need consists inside the script tag 'window.renderSearchSection'
-      // Cheerio returns the result as an object, so we convert it to an array because we can use array methods
-      const scripts = $("script").toArray();
+        // from view-source page, the data we need consists inside the script tag 'window.renderSearchSection'
+        // Cheerio returns the result as an object, so we convert it to an array because we can use array methods
+        const scripts = $('script').toArray();
 
-      scripts.find(script => {
-        if (script.children[0] !== undefined) {
-          if (script.children[0].data !== undefined) {
-            if (
-              script.children[0].data.includes("window.renderSearchSection")
-            ) {
-              let searchResults = script.children[0].data;
-              // change the format to be json
-              const startIndex = searchResults.indexOf("(");
-              let results = eval(searchResults.substring(startIndex));
-              this.setState({ listings: results.listings });
-              // console.log(results);
+        scripts.find(script => {
+          if (script.children[0] !== undefined) {
+            if (script.children[0].data !== undefined) {
+              if (
+                script.children[0].data.includes('window.renderSearchSection')
+              ) {
+                let searchResults = script.children[0].data;
+                // change the format to be json
+                const startIndex = searchResults.indexOf('(');
+                let results = eval(searchResults.substring(startIndex));
+                this.setState({ listings: results.listings });
+                // console.log(results);
+              }
             }
           }
-        }
-      });
+        });
+      } else {
+        console.error('Invalid URL!');
+      }
     });
   }
 
@@ -48,12 +52,14 @@ class AlertListings extends Component {
     return (
       <div>
         {this.state.listings.map(listing => {
-          return <ListingCard 
-            price={listing.price}
-            city={listing.city}
-            createdOn={listing.createTime}
-            photo={listing.photo}
-          />
+          return (
+            <ListingCard
+              price={listing.price}
+              city={listing.city}
+              createdOn={listing.createTime}
+              photo={listing.photo}
+            />
+          );
         })}
       </div>
     );
