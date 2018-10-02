7f6f35aeb2d5d623f7f7ad14c73b68ac04c29bbe
Tyson Williams <tyson.j.williams@gmail.com>
2018-10-02 10:03:11 -0600

    Create a page not found component

---
+++ client/src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'semantic-ui-react';
+
+const DivStyle = styled.div`
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  text-align: center;
+`;
+
+const PageNotFound = (props) => {
+  const goToHome = () => {
+    // const homePage = 'http://localhost:3000/'; // for testing on local machine
+    props.history.push('/');
+  }
+
+  return (
+    <DivStyle>
+      <h1>Oh no! Page not found, or you're not signed in.</h1>
+      <Button  color="grey" size="massive" onClick={goToHome}>Home</Button>
+    </DivStyle>
+  );
+};
+
+export default PageNotFound;
