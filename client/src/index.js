08c8ecd3443dc3974bf55100f2fb009169834691
Tyson Williams <tyson.j.williams@gmail.com>
2018-10-03 15:35:17 -0600

    Wrap App component with React Router Switch Render App via React Router's Route Pass location history into App as a prop

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Switch>
+      <Route path='/' render={(props) => <App history={props.history}/>} />
+    </Switch>
+    {/* <App component={(props) => <PageNotFound history={props.history}/>} /> */}
   </Router>,
   document.getElementById('root')
 );
