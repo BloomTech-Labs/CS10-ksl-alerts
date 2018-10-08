f228601aa854ab85848e2458df11382fc96e666c
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-08 13:39:48 -0700

    Add 'active' prop to Menu.Item

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Header, Menu, Icon } from 'semantic-ui-react';
-import SignOutButton from '../ButtonComponents/SignOutButton/SignOutButton.js';
+import { Container, Menu, Icon } from 'semantic-ui-react'; // eslint-disable-line
+import SignOutButton from '../ButtonComponents/SignOutButton/SignOutButton.js'; //eslint-disable-line
 import './TopNav.css';
 
 const TopNav = props => {
@@ -18,19 +18,39 @@ const TopNav = props => {
     <div className="navigationmenu">
       <Menu borderless className="TopNav" size="large">
         <Container className="TopNavWrapper" fluid>
-          <Menu.Item as={Link} to="/feed" className="MenuLink">
+          <Menu.Item
+            as={Link}
+            to="/feed"
+            className="MenuLink"
+            active={currentLocation === '/feed'}
+          >
             <Icon name="feed" position="left" />
             Alerts
           </Menu.Item>
-          <Menu.Item as={Link} to="/createAlert" className="MenuLink">
+          <Menu.Item
+            as={Link}
+            to="/createAlert"
+            className="MenuLink"
+            active={currentLocation === '/createAlert'}
+          >
             <Icon name="add" position="left" />
             Create Alert
           </Menu.Item>
-          <Menu.Item as={Link} to="/billing" className="MenuLink">
+          <Menu.Item
+            as={Link}
+            to="/billing"
+            className="MenuLink"
+            active={currentLocation === '/billing'}
+          >
             <Icon name="credit card" position="left" />
             Billing
           </Menu.Item>
-          <Menu.Item as={Link} to="/settings" className="MenuLink">
+          <Menu.Item
+            as={Link}
+            to="/settings"
+            className="MenuLink"
+            active={currentLocation === '/settings'}
+          >
             <Icon name="settings" position="left" />
             Settings
           </Menu.Item>
