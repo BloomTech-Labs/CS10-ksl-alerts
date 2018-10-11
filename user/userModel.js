c8e2a8cf430277df79ff5de7be6dbcbc80a6c7ac
Tyson Williams <tyson.j.williams@gmail.com>
2018-10-11 16:20:22 -0600

    Update user model Change customerId to stripeId Change subscription to use only a String name for 'free' or 'premium'

--- user/userModel.js
+++ user/userModel.js
@@ -2,65 +2,66 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 // user model
-const UserSchema = new mongoose.Schema ({
-	email: {
+const UserSchema = new mongoose.Schema({
+  email: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true
-	},
-	password: {
+    lowercase: true,
+  },
+  password: {
     type: String,
     required: true,
-    minlength: 6
-	},
-	// one to many relationship - one user can save many urls.
-  queries: [{
-    title: {
+    minlength: 6,
+  },
+  // one to many relationship - one user can save many urls.
+  queries: [
+    {
+      title: {
         type: String,
         required: true,
-    },
-    url: {
+      },
+      url: {
         type: String,
-        required: true
-    }
-	}],
-  customerId: {
+        required: true,
+      },
+    },
+  ],
+  stripeId: {
     type: String,
-    required: false
-	},
-
-	subscription: {
-		type: String,
-		required: true,
-		description: String,
-		subscriptionPeriod: Number
-	},
+    required: false,
+  },
+  subscription: {
+    type: String,
+    required: true,
+    // description: String,
+    // subscriptionPeriod: Number,
+  },
 });
 
-// hashing middleware 
-UserSchema.pre('save', function(next) {
-    bcrypt.hash(this.password, 10, (err, hash) => {
-        if (err) {
-            return next(err);
-        }
-        this.password = hash;
-        next();
-    });
+// hashing middleware
+UserSchema.pre("save", function(next) {
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  });
 });
 
 // Hashes new password before updating User model
-UserSchema.pre('findOneAndUpdate', function(next) {
+UserSchema.pre("findOneAndUpdate", function(next) {
   if (this._update.password)
-    this._update.password = bcrypt.hashSync(this._update.password, 10)
+    this._update.password = bcrypt.hashSync(this._update.password, 10);
   next();
-})
+});
 
 // compare the password when user login.
 UserSchema.methods.validatePassword = function(passwordGuess) {
-    return bcrypt.compare(passwordGuess, this.password);
+  return bcrypt.compare(passwordGuess, this.password);
 };
 
-const userModel = mongoose.model('User', UserSchema);
+const userModel = mongoose.model("User", UserSchema);
 
 module.exports = userModel;
