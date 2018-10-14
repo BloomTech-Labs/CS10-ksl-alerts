c2ce459ad16309aedbdde6e53b4d8cd624f2277a
Tyson Williams <tyson.j.williams@gmail.com>
2018-10-13 22:03:27 -0600

    Update stripe.js file Create method to update user's subscription in database Change successPayment function to newly created updateSubscription function Pass in updateSubscriptionState to onToken function Minor changes to code organization

--- client/src/components/Billing/Stripe/stripe.js
+++ client/src/components/Billing/Stripe/stripe.js
@@ -2,23 +2,41 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 
-const CURRENCY = 'USD';
 const PAYMENT_SERVER_URL = 'http://localhost:8000/api/payments';
 const STRIPE_PUBLISHABLE = 'pk_test_bS41glL52zljnAT32tZubD7r';
-
-
+const CURRENCY = 'USD';
 
 const fromDollarToCent = amount => amount * 100;
 
-const successPayment = data => {
-  alert('Payment Successful');
+const updateSubscription = (subscription, updateSubscriptionState) => {
+  const token = localStorage.getItem('token');
+  const id = localStorage.getItem('id');
+    const requestOptions = {
+      headers: {
+        Authorization: token
+      }
+    };
+
+    axios
+      .put(
+        `${process.env.REACT_APP_BACKEND_URL}/user/updateSubscription`,
+        { id, subscription },
+        requestOptions
+      )
+      .then(res => {
+        updateSubscriptionState(subscription);
+      })
+      .catch(err => {
+        console.log(err);
+      });
 };
 
-const errorPayment = data => {
+const errorPayment = error => {
   alert('Payment Error');
+  console.error(error);
 };
 
-const onToken = (amount, description) => token =>
+const onToken = (amount, description, updateSubscriptionState) => token => {
   axios
     .post(
       PAYMENT_SERVER_URL,
@@ -30,19 +48,20 @@ const onToken = (amount, description) => token =>
       },
     )
     .then(response => {
-      successPayment();
+      alert('Payment Successful!');
+      updateSubscription('premium', updateSubscriptionState);
     })
     .catch(error =>{
-      errorPayment()
-      console.error(error);
+      errorPayment(error);
     });
+}
 
-const Checkout = ({ name, description, amount }) => (
+const Checkout = ({ name, description, amount, updateSubscriptionState }) => (
   <StripeCheckout
     name={name}
     description={description}
     amount={fromDollarToCent(amount)}
-    token={onToken(amount, description)}
+    token={onToken(amount, description, updateSubscriptionState)}
     currency={CURRENCY}
     stripeKey={STRIPE_PUBLISHABLE}
   />
